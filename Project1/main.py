dc7124629affa029e6fae5c44b131addbc839cf0
Faris Dinarevic <e12534806@student.tuwien.ac.at>
2025-10-29 23:51:51 +0100

    did stuff

--- Project1/main.py
+++ Project1/main.py
@@ -3,33 +3,44 @@ from os import system as system
 
 
 
-Gate = input("Choose gate (AND/OR/NOT/NAND/NOR/XOR) \nTo exit type 'EXIT'\n").upper()
-
-
-if Gate ==("EXIT"):
-    exit()
-elif Gate not in ["AND","OR","NOT","NAND","NOR","XOR"]:
-    print("Please select a valid operation")
-    system(r"python .\Project\main.py")
-    exit()
-
-
+Gate = 0#input("Choose gate (AND/OR/NOT/NAND/NOR/XOR) \nTo exit type 'EXIT'\n").upper()
+def GateResult(Gate):
     
-a= gate.getBoolInput("Enter input 1: ")
-b= gate.getBoolInput("Enter input 2: ")
+    if Gate ==("EXIT"):
+        exit()
+    elif Gate not in ["AND","OR","NOT","NAND","NOR","XOR"]:
+        print("Please select a valid operation")
+        system(r"python .\Project\Project1\main.py")
+        exit()
 
-match Gate:
-    case "AND":
-        print("Output:", gate.AndGate(a,b).output())
-    case "OR":
-        print("Output:", gate.OrGate(a,b).output())
-    case "NOT":
-        print("Output:", gate.NotGate(a,b).output())
-    case "NAND":
-        print("Output:", gate.NandGate(a,b).output())
-    case "NOR":
-        print("Output:", gate.NorGate(a,b).output())
-    case "XOR":
-        print("Output:", gate.XorGate(a,b).output())
 
         
+    a= gate.getBoolInput("Enter input 1: ")
+    b= gate.getBoolInput("Enter input 2: ")
+    
+    global Result;
+   
+    match Gate:
+        case "AND":
+            print("Output:", gate.AndGate(a,b).output())
+            Result = gate.AndGate(a,b).output()
+        case "OR":
+            print("Output:", gate.OrGate(a,b).output())
+            Result = gate.OrGate(a,b).output()
+        case "NOT":
+            print("Output:", gate.NotGate(a,b).output())
+            Result = gate.NotGate(a,b).output()
+        case "NAND":
+            print("Output:", gate.NandGate(a,b).output())
+            Result = gate.NandGate(a,b).output()
+        case "NOR":
+            print("Output:", gate.NorGate(a,b).output())
+            Result = gate.NorGate(a,b).output()
+        case "XOR":
+            print("Output:", gate.XorGate(a,b).output())
+            Result = gate.XorGate(a,b).output()
+        
+    return Result , Gate
+
+
+GateResult()
--- Project1/tkint.py
+++ Project1/tkint.py
@@ -1,11 +1,11 @@
 # Program to make a simple 
 # login screen  
 
-
+from tkinter import ttk
 import tkinter as tk
-#import main 
+from main import *
 root=tk.Tk()
-
+root.title("GATE")
 # setting the windows size
 root.geometry("600x400")
  
@@ -13,6 +13,9 @@ root.geometry("600x400")
 # for storing gate1 and gate2ord
 gate1_var=tk.StringVar()
 gate2_var=tk.StringVar()
+gate_type_var=tk.StringVar()
+mainframe = ttk.Frame(root, padding=(3, 3, 12, 12))
+mainframe.grid(column=0, row=0, sticky=(tk.N, tk.W,tk.E,tk.S))
 
  
 # defining a function that will
@@ -22,12 +25,19 @@ def submit():
 
     gate1=gate1_var.get()
     gate2=gate2_var.get()
+    gateType = gate_type_var.get()
+    #if((type(gate1) != int) or (type(gate2) != int) or gateType not in ['AND', 'OR', 'NOT', 'NAND', 'NOR', 'XOR', 'XNOR']):
+   #     print("Invalid Input"
+     #   )
+    #    return 0
     
     print("The gate1 is : " + gate1)
-    print("The gate2ord is : " + gate2)
-    
+    print("The gate2 is : " + gate2)
+    print(   "The gate type is : " + gateType)
     gate1_var.set("")
     gate2_var.set("")
+    gate_type_var.set("")
+    return gate1, gate2, gateType
     
     
 # creating a label for 
@@ -38,12 +48,16 @@ gate1_label = tk.Label(root, text = 'gate1', font=('calibre',10, 'bold'))
 # gate1 using widget Entry
 gate1_entry = tk.Entry(root,textvariable = gate1_var, font=('calibre',10,'normal'))
  
-# creating a label for gate2ord
+# creating a label for gate2
 gate2_label = tk.Label(root, text = 'gate2', font = ('calibre',10,'bold'))
  
 # creating a entry for gate2ord
-gate2_entry=tk.Entry(root, textvariable = gate2_var, font = ('calibre',10,'normal'), show = '*')
+gate2_entry=tk.Entry(root, textvariable = gate2_var, font = ('calibre',10,'normal'))
  
+ 
+ 
+gateType_entry_label = tk.Label(root, text = 'gate type', font = ('calibre',10,'bold'))
+gateType_entry=tk.Entry(root, textvariable = gate_type_var, font = ('calibre',10,'normal'))
 # creating a button using the widget 
 # Button that will call the submit function 
 sub_btn=tk.Button(root,text = 'Submit', command = submit)
@@ -55,7 +69,9 @@ gate1_label.grid(row=0,column=0)
 gate1_entry.grid(row=0,column=1)
 gate2_label.grid(row=1,column=0)
 gate2_entry.grid(row=1,column=1)
-sub_btn.grid(row=2,column=1)
+gateType_entry_label.grid(row=3,column=0)
+gateType_entry.grid(row=3,column=1)
+sub_btn.grid(row=4,column=1)
  
 # performing an infinite loop 
 # for the window to display
